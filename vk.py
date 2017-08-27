650e23f1b7df773829a1589d6a68955d839872b9
Migachev Alexey <reklama.analitika@gmail.com>
2017-08-27 18:07:25 +0300

    prefatory

--- vk.py
+++ vk.py
@@ -1,50 +1,79 @@
 import time
 import requests
-from urllib.parse import urlencode
+import json
 
-# url_authorize = 'https://oauth.vk.com/authorize'
-# auth_date = {'client_id': 6112358, 'display': 'mobile', 'redirect_uri': 'https://oauth.vk.com/blank.html',
-#              'scope': 'friends', 'v':'5.67','response_type':'token'}
-# ulr_token = ('?'.join((url_authorize, urlencode(auth_date))))
-
-
-with open('../vk_token.txt') as f:
+with open('./vk_token.txt') as f:
     token = f.read()
 
 
+USER_ID = input("Введите имя или ID пользователя: ")
+
+
 def account_friends(token):
-    params = {'access_token': token, 'v': '5.67', 'fields': 'first_name,last_name'}
-    r_profile = requests.get('https://api.vk.com/method/account.getProfileInfo', params).json()
+    params = {'access_token': token, 'v': '5.67', 'fields': 'first_name,last_name', 'user_id': USER_ID, 'extended': 1}
+    r_profile = requests.get('https://api.vk.com/method/users.get', params).json()
     r_friends = requests.get('https://api.vk.com/method/friends.get', params).json()
-    account_friends = []
-    friends_id = []
-    for i in r_friends['response']['items']:
-        account_friends.append(f"{i['last_name']} {i['first_name']}")
-        friends_id.append(i['id'])
+    r_groups = requests.get('https://api.vk.com/method/groups.get', params).json()
 
-    print('Список друзей аккаунта {} {}'.format(r_profile['response']['first_name'], r_profile['response']['last_name']),
-          '\nВсего {} человека - {}'.format(r_friends['response']['count'], ', '.join(account_friends)))
-    return friends_id
+    acc_friends = [{i['last_name']: i['first_name']} for i in r_friends['response']['items']]
+    friends_id = [i['id'] for i in r_friends['response']['items']]
+    groups_id = [i['id'] for i in r_groups['response']['items']]
+
+    print('Список друзей пользователя {} {}'.format(r_profile['response'][0]['first_name'],
+                                                    r_profile['response'][0]['last_name']),
+          '\nВсего {} человека - {}'.format(r_friends['response']['count'], acc_friends))
+    print('Группы, в которых состоит наш пользователь: {}.\nВсего групп - {}.\n'.format(
+        '; '.join([i['name'] for i in r_groups['response']['items']]), r_groups['response']['count']))
+    return friends_id, groups_id
 
 
-def friends_friends(token):
+def groups_friends(token):
+    friends_id, groups_id = account_friends(token)
+    print('\nОжидайте, данные обрабатываются ...\nНомер шага соответсвует номеру друга и их не может быть более чем {}'.format(
+        len(friends_id)))
     counter = 0
-    set_fridens_fridens = set()
-    friends_id = account_friends(token)
-    with open('friends_friends.txt', 'w', encoding='utf-8') as f:
-        print('\nОжидайте, данные записываются в файл {} ...'.format(f.name))
-        for i in friends_id:
-            params = {'access_token': token, 'v': '5.67', 'fields': 'first_name,last_name', 'user_id': i}
-            r_user_profil = requests.get('https://api.vk.com/method/users.get', params).json()
-            r_user_fridens = requests.get('https://api.vk.com/method/friends.get', params).json()
-            time.sleep(1)
-            counter += r_user_fridens['response']['count']
-            f.write('{} {} - {}друзей. \n'.format(r_user_profil['response'][0]['first_name'], r_user_profil['response'][0]['last_name'], r_user_fridens['response']['count']))
-            for i in r_user_fridens['response']['items']:
-                f.write(i['first_name']+' '+i['last_name']+', ')
-                set_fridens_fridens |= {i['id']}
-            f.write('\n---------------\n')
-        f.write('\nОбщее колво всех друзей - {}. \nУникальных друзей - {}.'.format(counter, len(set_fridens_fridens)))
-    print('Данные успешно записаны\n', '\nОбщее колво всех друзей - {}. \nУникальных друзей - {}.'.format(counter, len(set_fridens_fridens)))
+    for j, i in enumerate(friends_id, start=1):
+        try:
+            params = {'access_token': token, 'v': '5.67', 'fields': 'first_name,last_name', 'user_id': i, 'extended': 1}
+            r_groups = requests.get('https://api.vk.com/method/groups.get', params).json()
+            time.sleep(0.4)
+            friend_groups_id = [i['id'] for i in r_groups['response']['items']]
+            groups_id = set(groups_id) - set(friend_groups_id)
+            if counter != len(groups_id):
+                counter = len(groups_id)
+                print(f'Шаг {j}. Колво индивидуальных групп - {counter}')
+                print(f'Осталось обработать {len(friends_id)-j}')
+            if len(groups_id) == 0:
+                break
+        except KeyError:
+            continue
+    print('----------------------------------')
+    return groups_id
 
-friends_friends(token)
+
+def output_data(token):
+
+    counter = groups_friends(token)
+    if len(counter) > 0:
+        print('Данные успешно обработаны\n', 'Колво индивидуальных групп - {}шт.\n'.format(len(counter)))
+    else:
+        print('Данные успешно обработаны\n Индивидуальных групп НЕТ!')
+
+    individ_groups = requests.get('https://api.vk.com/method/groups.getById',
+                            {'group_ids': ','.join(list(map(str, counter))), 'fields': 'members_count'}).json()
+
+    for i in individ_groups['response']:  # Вот тут у меня ерунда. 2-мя способами сделал и ни один мне не нравится!!
+        del (i['screen_name'], i['is_closed'], i["photo"], i["photo_medium"], i["photo_big"], i["type"])
+        # list.append({'gid': i['gid'], 'name':i['name'],'members_count': i['members_count'] })
+
+    with open('individual_groups.json', 'w', encoding='utf-8') as f:
+        json.dump(individ_groups['response'], f, indent=2, ensure_ascii=False)
+        print('Индивидуальные группы записаны в файл {}'.format(f.name))
+
+
+def main():
+    output_data(token)
+
+
+if __name__ == '__main__':
+    main()
